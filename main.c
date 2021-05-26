4f77711595b6fc1529b7c28bec6dc170e254b650
jairGil <salasun.jg@gmail.com>
2021-05-26 17:36:40 -0500

    main

--- main.c
+++ main.c
@@ -13,11 +13,19 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include "funciones.c"
 
 /*
  * 
  */
 int main(int argc, char** argv) {
+    
+    configuracion config;
+    tablero tab;
+
+    pideConfiguracion(&config);
+    creaTablero(&config, &tab);
+    llenaTablero(&config, &tab);
 
     return (EXIT_SUCCESS);
 }
